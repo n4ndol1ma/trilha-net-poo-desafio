288d7e7d300dfa9615f9863cfc6474ff168969be
n4ndol1ma <fernlimadasilva@gmail.com>
2023-11-24 09:09:36 -0500

    Entrega de desafio bootcamp - Decola Tech 2024

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,21 +2,25 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Numero { get; }
+        public string Modelo { get; }
+        public string Imei { get; }
+        public int Memoria { get;}
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
-        public void Ligar()
+        public virtual void Ligar()
         {
             Console.WriteLine("Ligando...");
         }
 
-        public void ReceberLigacao()
+        public virtual void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
         }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
-﻿using DesafioPOO.Models;
+﻿using System.Net;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "1234567", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\nSmartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "678910", modelo: "Modelo 2", imei: "22222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Twitter");
